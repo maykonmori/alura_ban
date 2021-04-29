06bd82686fd23461692b9075db9805a844bd6942
Maykon Mori <maykonmori@gmail.com>
2021-04-29 14:32:51 -0300

    att estudo

--- main.go
+++ main.go
@@ -9,11 +9,26 @@ type ContaCorrente struct {
 	saldo         float64
 }
 
+func (c *ContaCorrente) Sacar(valorDoSaque float64) string {
+	podeSacar := valorDoSaque > 0 && valorDoSaque <= c.saldo
+
+	if podeSacar {
+		c.saldo -= valorDoSaque
+		return "Saque realizado com sucesso"
+	} else {
+		return "Saldo insuficiente"
+	}
+}
+
 func main() {
 
-	contaDoGuilherme := ContaCorrente{titular: "Guilherme", numeroAgencia: 589, numeroConta: 123456, saldo: 125.5}
-	fmt.Println(contaDoGuilherme)
+	contaDaSilvia := ContaCorrente{}
+	contaDaSilvia.titular = "Silvia"
+	contaDaSilvia.saldo = 500
 
-	contaDaBruna := ContaCorrente{"Bruna", 222, 111222, 200}
-	fmt.Println(contaDaBruna)
+	fmt.Println(contaDaSilvia.saldo)
+
+	fmt.Println(contaDaSilvia.Sacar(200))
+	fmt.Println(contaDaSilvia.saldo)
+
 }
